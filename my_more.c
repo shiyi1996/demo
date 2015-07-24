093a129680e02d7e04e2770a8787d2982c211368
shiyi <shiyi19960604@gmail.com>
2015-07-24 08:51:01 +0800

    实现文件比例

--- my_more.c
+++ my_more.c
@@ -15,15 +15,16 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
-#define PAGENUM 10
+
+#define PAGENUM 10 //定义一屏为10行
 
 void do_more(int fd);
-int see_more(FILE  *fp, int sum, int now);
+int see_more(  int sum, int now);
 
 int main( int argc, char **argv )
 {
     int fd;
-    if( argc == 1 )
+    if( argc == 1 )//参数不合法则返回
     {
         return 0;
     }
@@ -48,28 +49,20 @@ int main( int argc, char **argv )
 
 void do_more(int fd)
 {
-    char str[256];
-    char *p = str;
-    int page_num = 0;
+    char str[3];
+    int page_num = 0;//保存每次需要跳转的行数
     int rel;
-    int page_now;
-    int page_sum;
-    FILE *fp_tty;
+    int page_now = 0;//保存当前行数（初始为0）
+    int page_sum;//保存总行数
+ 
 
-    rel = lseek(fd,0,SEEK_END);
-    page_sum = lseek(fd,0,SEEK_CUR);
-    rel = lseek(fd,0,SEEK_SET);
-
-
-    fp_tty = fopen("/dev/tty","r");
-    if( fp_tty == NULL )
-    {
-        exit(1);
-    }
+    lseek(fd,0,SEEK_END);//移动文件指针至文件末
+    page_sum = lseek(fd,0,SEEK_CUR);//计算与文件头偏移量，即文件总行数
+    lseek(fd,0,SEEK_SET);//移动文件指针至文件头
 
     while( page_now<page_sum )
     {
-        while(read(fd,str,1))
+        while(read(fd,str,1))//每次读一行
         {
             if(str[0] =='\n')
             {
@@ -78,11 +71,11 @@ void do_more(int fd)
             }
             printf("%c",str[0]);
         }
-        page_now = lseek(fd, 0, SEEK_CUR);
+        page_now = lseek(fd, 0, SEEK_CUR);//实时更新已读行数
         if( page_num == PAGENUM )
         {
-            rel = see_more(fp_tty, page_sum, page_now);
-            printf("\033[1A\033[K");
+            rel = see_more( page_sum, page_now);//获取用户输入
+            printf("\033[1A\033[K");//将光标上移一行，并清空该行内容，用以覆盖提示信息
             if( rel == 0 )
             {
                 break;
@@ -96,12 +89,12 @@ void do_more(int fd)
     }
 }
 
-int see_more(FILE *fp, int sum,int now)
+int see_more(int sum,int now)
 {
     int rel;
-    printf("\033[7m --更多-- %.2f%% \033[0m",now*100.0/sum);
+    printf("\033[7m --更多-- enter翻行 backspace翻页 q退出  %.2f%% \033[0m",now*100.0/sum);//反显输出提示信息以及文件已读比例
 
-    while( (rel = getc(fp)) != EOF )
+    while(rel = getchar())
     {
         if(rel == 'q')
         {
